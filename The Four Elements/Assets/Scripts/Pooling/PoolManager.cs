3a2cfc261100b022a02a93a3726992fe3989b671
Barış <46918905+Baris-Yildiz@users.noreply.github.com>
2025-04-24 17:25:11 +0300

    spell casting for fire and ice
    * spell manager
    * pooling of spells

---
+++ The Four Elements/Assets/Scripts/Pooling/PoolManager.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private GameObject[] pooledObjects;
+    private int currentObjectIndex = 0;
+    public Action<GameObject> OnSpawnPooledObject;
+    public GameObject PoolObject = null;
+    public int PoolAmount = 0;
+
+    void Start()
+    {
+        SetPooledObjects();
+    }
+
+    private void SetPooledObjects()
+    {
+        pooledObjects = new GameObject[PoolAmount];
+
+        for (int i = 0; i < pooledObjects.Length; i++)
+        {
+            GameObject pooledObject = Instantiate(PoolObject, transform);
+            pooledObject.SetActive(false);
+            pooledObjects[i] = pooledObject;
+        }
+    }
+
+    public GameObject SpawnPooledObject(Vector3 position, Quaternion rotation)
+    {
+        GameObject currentObject = GetCurrentObject();
+
+        if (currentObject == null)
+        {
+            return null;
+        }
+
+        OnSpawnPooledObject?.Invoke(currentObject);
+
+        currentObject.SetActive(false);
+        currentObject.transform.SetPositionAndRotation(position, rotation);
+        currentObject.SetActive(true);
+        
+        SetCurrentObject();
+        return currentObject;
+    }
+
+    public GameObject GetCurrentObject()
+    {
+        return pooledObjects[currentObjectIndex];
+    }
+
+    private void SetCurrentObject()
+    {
+        currentObjectIndex = (currentObjectIndex + 1) % pooledObjects.Length;
+    }
+}
---
+++ The Four Elements/Assets/Scripts/Spell/SpellManager.cs
@@ -0,0 +1,100 @@
+using Cinemachine;
+using UnityEngine;
+
+public class SpellManager : MonoBehaviour
+{
+    public GameObject[] spells;
+    private int currentSpellIndex;
+    private GameObject currentSpell;
+    public int CurrentSpellIndex
+    {
+        get
+        {
+            return currentSpellIndex;
+        }
+        set
+        {
+            currentSpellIndex = value;
+            currentSpell = spells[currentSpellIndex];
+        }
+    }
+
+    public Transform dummy;
+    public GameObject player;
+    public Transform raycastStartTransform;
+
+    public CinemachineVirtualCamera playerCamera;
+    public LayerMask LayerMask;
+    public GameObject InvisibleSpellWall;
+
+    private PoolManager[] poolManagers;
+    private PoolManager invisibleWallPoolManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        poolManagers = gameObject.GetComponentsInChildren<PoolManager>();
+        invisibleWallPoolManager = GameObject.Find("InvisibleWallPool").GetComponent<PoolManager>();
+        CurrentSpellIndex = 0;
+        LayerMask &= ~LayerMask.GetMask("Building", "Ground");
+        print(LayerMask);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SpawnSpellObject(dummy);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SwitchSpellType();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ShootForward();
+        }
+    }
+
+    private void SpawnSpellObject(Transform target)
+    {
+        poolManagers[CurrentSpellIndex].OnSpawnPooledObject = (GameObject spellObj) =>
+        {
+            Transform[] targetArray = new Transform[1];
+            targetArray[0] = target;
+            print(target);
+            spellObj.GetComponent<MagicFX5_EffectSettings>().Targets = targetArray;
+        };
+        poolManagers[CurrentSpellIndex].SpawnPooledObject(raycastStartTransform.position, Quaternion.identity);
+    }
+
+    private void SwitchSpellType()
+    {
+        CurrentSpellIndex = (CurrentSpellIndex + 1) % spells.Length;
+    }
+
+    private void ShootForward()
+    {
+        RaycastHit hit;
+        Debug.DrawLine(playerCamera.transform.position, playerCamera.Follow.position, Color.black);
+        // check for burnable
+        if (Physics.Raycast(raycastStartTransform.position, playerCamera.Follow.position - playerCamera.transform.position, out hit, Mathf.Infinity, LayerMask))
+        {
+            SpawnSpellObject(hit.transform);
+        }
+        else
+        {
+            //check for non burnable
+            RaycastHit hitNoMask;
+            if (Physics.Raycast(raycastStartTransform.position, playerCamera.Follow.position - playerCamera.transform.position, out hitNoMask, Mathf.Infinity))
+            {
+                GameObject spawnedWall = invisibleWallPoolManager.SpawnPooledObject(hitNoMask.point, Quaternion.identity);// Instantiate(InvisibleSpellWall, hitNoMask.point, Quaternion.identity);
+                SpawnSpellObject(spawnedWall.transform);
+            }
+        }
+    }
+}
