6e93b5041ab07a8c52581e4109af4ff3442758f9
cetinsaid <137168667+cetinsaid@users.noreply.github.com>
2025-05-19 01:23:43 +0300

    added frame

--- The Four Elements/Assets/Scripts/UI/ElementSwitchUI.cs
+++ The Four Elements/Assets/Scripts/UI/ElementSwitchUI.cs
@@ -7,18 +7,22 @@ public class ElementSwitchUI : MonoBehaviour
     private PlayerElementManager switcher;
     [SerializeField] private Image fireSprite;
     [SerializeField] private Image fireSpriteBackGround;
+    [SerializeField] private Image fireFrame;
     private Color originalColorFire;
     
     [SerializeField] private Image waterSprite;
     [SerializeField] private Image waterSpriteBackGround;
+    [SerializeField] private Image waterFrame;
     private Color originalColorWater;
     
     [SerializeField] private Image windSprite;
     [SerializeField] private Image windSpriteBackGround;
+    [SerializeField] private Image windFrame;
     private Color originalColorWind;
     
     [SerializeField] private Image soilSprite;
     [SerializeField] private Image soilSpriteBackGround;
+    [SerializeField] private Image soilFrame;
     private Color originalColorSoil;
     
     [SerializeField]private Color backGroundColor = Color.black;
@@ -54,24 +58,44 @@ public class ElementSwitchUI : MonoBehaviour
                 windSpriteBackGround.color = backGroundColor;
                 waterSpriteBackGround.color =backGroundColor;
                 soilSpriteBackGround.color = backGroundColor;
+
+                fireFrame.color = originalColorFire;
+                waterFrame.color = Color.white;
+                windFrame.color = Color.white;
+                soilFrame.color = Color.white;
                 break;
             case ElementalType.WATER:
                 fireSpriteBackGround.color = backGroundColor;
                 windSpriteBackGround.color = backGroundColor;
                 waterSpriteBackGround.color = originalColorWater;
                 soilSpriteBackGround.color = backGroundColor;
+
+                fireFrame.color = Color.white;
+                waterFrame.color = originalColorWater;
+                windFrame.color = Color.white;
+                soilFrame.color = Color.white;
                 break;
             case ElementalType.WIND:
                 fireSpriteBackGround.color = backGroundColor;
                 windSpriteBackGround.color = originalColorWind;
                 waterSpriteBackGround.color = backGroundColor;
                 soilSpriteBackGround.color = backGroundColor;
+
+                fireFrame.color = Color.white;
+                waterFrame.color = Color.white;
+                windFrame.color = originalColorWind;
+                soilFrame.color = Color.white;
                 break;
             case ElementalType.SOIL:
                 fireSpriteBackGround.color = backGroundColor;
                 windSpriteBackGround.color = backGroundColor;
                 waterSpriteBackGround.color = backGroundColor;
                 soilSpriteBackGround.color = originalColorSoil;
+
+                fireFrame.color = Color.white;
+                waterFrame.color = Color.white;
+                windFrame.color = Color.white;
+                soilFrame.color = originalColorSoil;
                 break;
         }
     }
